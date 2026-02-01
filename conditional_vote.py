ee2fd2c0a2b78df7d3b5e287f8f667d60ea0ccad
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-01 13:11:38 +0530

    initial commit

---
+++ conditional_vote.py
@@ -0,0 +1,5 @@
+#CONDITIONAL OPERATORS. => checks the conditions and then give output according to the input.
+
+age = int(input("enter our age..."))
+if(age >= 18):
+    print("you can vote.\n you can drive also.")
