235d41804989823b2ff77bcc62a4df68e2b300c9
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-02 14:28:00 +0530

    initial commit

---
+++ taking_entries.py
@@ -0,0 +1,11 @@
+# ENTER THREE FAVOURITE MOVIES AND STORE THEM INTO LIST.
+list = []
+mov1 = input("enter first movie name : ")
+mov2 = input("enter second movie name : ")
+mov3 = input("enter third movie name : ")
+
+list.append(mov1)
+list.append(mov2)
+list.append(mov3)
+
+print("these are three favourite movies : ",list)
