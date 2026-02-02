6d0c0037c94f5f34363c8cbf19a852bfda1ffdf4
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-02 14:25:59 +0530

    python traffic_light file

---
+++ traffic_light.py
@@ -0,0 +1,9 @@
+light = input("enter the color of the traffic light : ")
+if(light == "red"):
+    print("you should have stopped there.")
+elif(light == "yellow"):
+    print("this warning signal.")
+elif(light == "green"):
+    print("you should go.")
+else:
+    print("wrong input.")
