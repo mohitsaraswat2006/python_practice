be5b5820be78b195d47b2baa94ad10676015b2d1
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-03 20:41:07 +0530

    updated dictionary

--- dictionary.py
+++ dictionary.py
@@ -6,7 +6,50 @@ info = {
     "age" : 24,
     "is adult" : True,
     "marks" : 95.3,
-    "subjects" : ["python","c", "javacsript", "html"]
+    "subjects" : ["python","c", "javacsript", "html"],
+    "marks" : ("maths","hindi","english","science") 
 }
 print(info)
 print(type(info))
+
+# to print individual values.
+print(info["marks"])
+print(info["name"])
+
+info["ram"] = "hanuman"
+info["surname"] = "saraswat"
+
+null_dict = {}
+null_dict["name"] = "apnacollege"
+print(null_dict)
+
+# NESTED DICTIONARIES.
+
+student = {
+    "name" : 6,
+    "subjects" : {
+        "phy" : 78,
+        "maths" : 45,
+        "hindi" : 98
+    }
+}
+print("student")
+
+# methods in dictionary.
+print(student.keys())
+print(list(student.keys()))
+print(len(student))
+
+# to print values.
+print(student.values())
+
+# returns all items.
+print(student.items())
+print(list(student.items()))
+
+# to return key.
+print(student.get("name"))
+
+# to update dict.
+student.update({"city":"delhi"})
+print(student)
