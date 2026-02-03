ad8847baf2db04fc2cf1ce6be277fbaf1e77ba6e
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-03 19:58:06 +0530

    dictionaries in python

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+# STORES VALUES IN KEY:VALUE PAIRS INSIDE CURLY BRAKET{}.
+info = {
+    "key" : "value",
+    "name" : " mohit saraswat",
+    "learning " : "python",
+    "age" : 24,
+    "is adult" : True,
+    "marks" : 95.3,
+    "subjects" : ["python","c", "javacsript", "html"]
+}
+print(info)
+print(type(info))
