f9f2b23815aa62bd75713c97249cdb96fd05d666
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-07 16:30:04 +0530

    practicing loops

---
+++ print1 to 100.py
@@ -0,0 +1,12 @@
+# print 1 to 100 numbers by using while loop.
+i = 1
+while(i <= 100) :
+    print(i)
+    i += 1 
+
+
+# to print numbers 100 to 1 using whilw loop. 
+j = 100
+while(j >= 1) :
+    print(j)
+    j -= 1
