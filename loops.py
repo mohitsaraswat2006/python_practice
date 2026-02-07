37bac2ffa06e1ffe0edd3eeca18a9973607c8924
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-07 16:23:38 +0530

    practicing loop

---
+++ loops.py
@@ -0,0 +1,16 @@
+count = 1 
+while count <=5 :
+    print("hello")
+    count += 1
+
+print(count)
+
+i = 1
+while i <=10:
+    print(i)
+    i +=1  
+
+j = 5
+while j >= 1 :
+    print(j)
+    j -=1 
