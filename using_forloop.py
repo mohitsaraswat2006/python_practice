654c2839e65d95b7ecaf408c6aaec89798b188f0
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-09 20:51:28 +0530

    loops

---
+++ using_forloop.py
@@ -0,0 +1,11 @@
+list = [1,4,9,16,25,36,49,64,81,100]
+for val in list : 
+    print(val)
+
+#  SEARCH FOR A NUMBER X IN THIS TUPLE.
+num = [1,4,9,16,25,36,49,64,81,100]
+for el in num :
+    if(el == 49) :
+        print("number found") 
+        break
+    print(el)
