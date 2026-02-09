e7deb0d89b23d0180c44af789284d3c0e441ee68
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-09 20:53:55 +0530

    loops practice

---
+++ for_loops.py
@@ -0,0 +1,22 @@
+# EXAAMPLE.
+nums = [1,2,3,4,5,6]
+for el in nums:
+    print(el)
+
+
+# ANOTHER EXAMPLE.
+vaggies = ["POTATO","CUCUMBER","TOMATO","LADYFIINGER"]
+for val in vaggies :
+    print(val)
+
+
+# for tuples.
+tup = (1,2,3,4,5)
+for num in tup :
+    print(num)
+
+# for string.
+str = "mohit"
+for char in str :
+    print(char)
+ 
