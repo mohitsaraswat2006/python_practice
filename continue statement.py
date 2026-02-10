4d83d1c3019a99ed5d3f7c95d5ea5dcb54fd558f
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-10 18:43:36 +0530

    practicing loops

---
+++ continue statement.py
@@ -0,0 +1,17 @@
+j = 1
+while(j <= 10) :
+    if(j == 7) :
+        j += 1
+        continue
+    print(j)
+    j += 1
+
+# skip even numbers.
+i = 0
+while(i <= 20) :
+    if(i%2 == 0) :
+        i +=1
+        continue
+    print(i)
+    i += 1
+
