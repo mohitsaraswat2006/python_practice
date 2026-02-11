b2ae6b4756423ab1c9e69b7f6e33b12c8d6d27b5
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-11 21:32:56 +0530

    practicing functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+a = 5
+b = 10
+sum = a + b
+print(sum)
+
+# FUNCTION DEFINITION
+def cal_sum(a,b) :  # PARAMTERS
+    sum = a + b
+    return sum 
+
+print(cal_sum(4,5))   # FUNCTION CALL ; PARAMETERS
+
+def print_hello() :
+    print("hello")
+
+print_hello()
