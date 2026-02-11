2768d0e442bfcb0b09a6dfda017a2d8f4b814c66
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-11 21:34:11 +0530

    range in python

---
+++ practice_forwithrange.py
@@ -0,0 +1,14 @@
+# PRINT NUMBER FROM 0 TO 100.
+for num in range(101) :
+    print(num)
+
+
+# PRINT NUMBERS FROM 100 TO 0.
+for j in range(100,0,-1) :
+    print(j)
+
+
+# MULTIPLICATION ANY NUMBER.
+num = int(input("enter any nunber : "))
+for val in range(1,11) :
+    print(val*num)
