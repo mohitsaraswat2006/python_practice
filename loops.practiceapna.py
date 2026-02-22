0eb19dfec3ffc24d393a7e7a2c9dc0ac6bd50afd
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-22 10:52:45 +0530

    loops practice

---
+++ loops.practiceapna.py
@@ -0,0 +1,44 @@
+#====================================PRINT VOWEL COUNT IN A STRING.=============================
+# word = "artificial intelligence"
+# count = 0
+# for w in word :
+#     if(w=="a" or w=="e" or w=="i" or w=="o" or w=="u") :
+#         count+=1
+# print("vowel count is : ",count)    
+
+# ==========================PRINT SUM OF FIRST N NATURAL NUMBERS.===============================
+# n = int(input("enter the number : "))
+# i = 1
+# sum = 0
+# while(i<=n) :
+#     sum +=i
+#     i+=1
+# print("sum is : ",sum)    
+
+#==============================function to calculate factorial==================================
+# def fact(n) :
+#     fact = 1
+#     i = 1
+#     while(i<=n) :
+#         fact*=i
+#         i+=1
+#     return fact
+# print(fact(4))
+# print(fact(5))
+
+
+#ORRRRRR
+
+# def fact(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact*=i
+#         i+=1
+#     return fact
+# print(fact(4))
+# print(fact(5))
+
+
+
+
+
