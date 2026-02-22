c2f9f7d92488e6086938d82b68b987835be2d8f8
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-02-22 10:53:31 +0530

    revision

---
+++ python_revision.py
@@ -0,0 +1,188 @@
+# print("hello, world")
+
+# VARIABLES.
+# name = "mohit saraswat"
+# age = 21
+# pi = 3.14
+# print("my name is : ",name, "\nage is : ",age, "\npi value : ", pi)
+
+# DATA TYPES.
+# 1.int  2.strings 3.float 4.boolean 5.none
+ 
+
+# addd two numbers.
+# a = 3
+# b = 4
+# print("sum is : ",a + b)
+
+# ARITHMETIC OPERATORS.
+# a = 10
+# b = 5
+
+# print(a + b)
+# print(a - b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+# average of twop numbers
+# a = int(input("enter first number : "))
+# b = int(input("enter second number : "))
+# avg = (a + b)/2
+# print("avg is : ",avg)
+
+# gives output according to the age.
+# age = int(input("enter the age : "))
+# if(age < 13) :
+#     print("child.")
+# elif(age >= 13 and age < 18) :
+#     print("teenager.")
+# else :
+#     print("adult.")
+
+# SWAP TWO VARIABLES USING THIRD VARIABLE.
+# a = int(input("enter the value of first variable : "))
+# b = int(input("enter the value of third variable : "))
+ 
+# print("values of variables before swapping =>\nvalue of a = :",a, "\nvalue of b = : " ,b)
+# c = b
+# b = a
+# a = c
+# print("values of a and b after swapping is : => \nvalue of a = :",a , "\nvalue of b = :",b)
+
+
+# square and cube of a entered number.
+# m = int(input("enter first number : "))
+# square = m*m
+# cube = m*m*m
+# print("square of a number is : ", square)
+# print("cube of a number is : ", cube)
+
+
+# AREA OF RECTANGLE IS .
+# m = int(input("enter first side of rectangle is : "))
+# n = int(input("enter second side of a rectangle is : "))
+# area = m*n
+# print("area of rectangle is : ",area)
+
+
+#CONVERT TEMPERATURE FROM CELCIUS TO FARENHEIT.
+# temp = int(input("enter temperature in celcius : "))
+# temp2 = (temp*9/5) + 32
+# print("temperature in farenheit is : ",temp2)
+
+#NUMBER IS POSITIVE , NEGATIVE OR ZERO.
+# s = int(input("enter a number : "))
+# if(s > 0) :
+#     print("number is positive.")
+# elif(s < 0) :
+#     print("number is negative.")
+# else :
+#     print("number is zero.")
+
+
+# check year is leap or not.
+# year = int(input("enter an year : "))
+# if((year % 4 == 0 and year % 100 != 0)or year %400 == 0) :
+#     print("entered year is leap year.")
+# else :
+#     print("entered year is not leap year.")
+
+# LARGEST AMONG TWO NUMBERS.
+# a = int(input("enter first number : "))
+# b = int(input("enter second number : "))
+# if(a > b) :
+#     print("A is greater.")
+# else :
+#     print("B is greater.")
+
+# LARGEST AMONG THREE NUMBERS.
+# m = int(input("enter first number : "))
+# n = int(input("enter second number : "))
+# o = int(input("enter third number : "))
+# if(m > n and m > o) :
+#     print("A is greatest.")
+# elif(n > m and n > o) :
+#     print("B is greatest.")
+# else :
+#     print("C is greatest.")
+
+# CHECK NUMBER IS DIVISIBLE BY 5 AND 11.
+# num = int(input("enter number : "))
+# if(num % 5 == 0 and num % 11 == 0) :
+#     print("number is divisible by 5 and 11 both.")
+# else :
+#     print("number is not divisible by 5 and 11.")
+
+# GIVEN CHARACTER IS VOWEL OR CONSONENT.
+# n = input("enter a character (a-z) : ")
+# if(n == "a" or n == "e" or n =="i" or n == "o" or n == "u") :
+#     print("character is vowel.")
+# else :
+#     print("character is consonent.")
+
+#PROGRAM TO CALCULATE PERCENTAGE OF MARKS.
+# maths = int(input("enter the marks of maths : "))
+# hindi = int(input("enter the marks of hindi : "))
+# science = int(input("enter the marks of science : "))
+# english = int(input("enter the marks of english : "))
+# ssc = int(input("enter the marks of ssc : "))
+# per = ((maths + hindi + science + english + ssc)*100)/500
+# print("percentage pf marks : ",per)
+
+#ELIGIBLE FOR VOTING OR NOT.
+# age = int(input("enter your age : "))
+# if(age >= 18) :
+#     print("eligible for voting.")
+# else :
+#     print("not eligible for voting.")
+
+# CHECK NUMBER IS MULTIPLE OF 3 OR 7.
+# num = int(input("enter the number : "))
+# if(num % 3 == 0 or num % 7 == 0 ) : 
+#     print("number is multiple of 3 or 7")
+# else:
+#     print("not multiple of 3 or 7")
+
+#CALCULATE THE ELECTRICITY BILL BASED ON THE UNIT CHARGES.
+# units = int(input("enter units used : "))
+# if(units > 0 and units <= 100) :
+#     print("the charges are : ", units*5)
+# elif(units > 100 and units <= 200):
+#     print("the charges are : ", units*7) 
+# elif(units > 200) :
+#     print("the charges are : ",units*10)
+# else :
+#     print("wrong input.")
+
+#CALCULATE BODY MASS INDEX.
+# weight = float(input("enter your weight in kgs : "))
+# height = float(input("enter your height in mts : "))
+# BMI = weight/(height**2)
+# if(BMI < 18.5) :
+#     print("you are underweight.")
+# elif(BMI >= 18.5 and BMI < 25) :
+#     print("you have normal weight.")
+# elif(BMI >=25 and BMI <= 30) :
+#     print("you are overweight.")
+# else :
+#     print("obese.")
+
+# print("BMI is :",BMI)    
+
+#CALCULATE COMPOUND INTEREST.
+# p = int(input("enter the principal amount : "))
+# r = int(input("rate of interest : "))
+# t = int(input("time taken : "))
+# a = p*(1+r/100)**t
+# print("that's the total amount : ",a)
+# ci = a - p
+# print("compound interest is : ",ci)
+
+
+
+
+
+
+
