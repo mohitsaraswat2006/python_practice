95fadeb0ec95eef84deb97d9bff3dd45c3d66340
mohitsaraswat2006 <mohitsaraswat2006@gmail.com>
2026-03-11 09:57:26 +0530

    f-strings

---
+++ fstrings.py
@@ -0,0 +1,7 @@
+name = "mohit"
+age = 21
+college = "bsacet"
+section = "b"
+
+print("my name is",name,"and age",age,"from college",college,"in section",section)
+print(f"my name is{name} and age {age} from college {college} in section{section}")
